7ee17f275e76891f8afb3bfa6cb22fa097e3e8f1
Marc Altmann <marcaltmann@posteo.de>
2019-04-16 20:10:48 +0200

    Use Bootstrap

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,15 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    'ember-bootstrap': {
+      bootstrapVersion: 4,
+      importBootstrapFont: false,
+      importBootstrapCSS: false,
+      blacklist: [
+        'bs-popover',
+        'bs-accordion',
+      ],
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
