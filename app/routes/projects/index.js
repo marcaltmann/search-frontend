ec90ce8240a76d2c661fa0407b891028da8ba550
Marc Altmann <marcaltmann@posteo.de>
2019-04-16 11:58:53 +0200

    Remove Ember welcome page

--- app/routes/projects/index.js
+++ app/routes/projects/index.js
@@ -1,13 +1,8 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  async model() {
-    try {
-      let projects = await this.store.findAll('project');
-    } catch (e) {
-      console.log(e);
-    }
-    debugger;
+  model() {
+    let projects = this.store.findAll('project');
     return projects;
   },
 });
